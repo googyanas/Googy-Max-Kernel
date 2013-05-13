72ddc53f95a5b48e9c304faa729b4ac28e712dfb
googyanas <googyanas@gmail.com>
2013-05-13 13:36:44 +0000

    Wifi driver

--- drivers/net/wireless/bcmdhd/dhd_sec_feature.h
+++ drivers/net/wireless/bcmdhd/dhd_sec_feature.h
@@ -1,5 +1,8 @@
-/*
- * Customer HW 4 dependant file
+﻿/*
+ * Header file describing the internal (inter-module) DHD interfaces.
+ *
+ * Provides type definitions and function prototypes used to link the
+ * DHD OS, bus, and protocol modules.
  *
  * Copyright (C) 1999-2012, Broadcom Corporation
  *
@@ -21,11 +24,8 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * $Id: dhd_sec_feature.h$
+ * $Id: dhd_sec_feature.h 309548 2012-01-20 01:13:08Z $
  */
-
-#ifndef _dhd_sec_feature_h_
-#define _dhd_sec_feature_h_
 
 /* PROJECTS */
 
@@ -33,127 +33,91 @@
 	|| defined(CONFIG_MACH_SAMSUNG_ESPRESSO_10)
 #define READ_MACADDR
 #define HW_OOB
-#endif /* CONFIG_MACH_SAMSUNG_ESPRESSO && CONFIG_MACH_SAMSUNG_ESPRESSO_10 */
+#endif
 
 /* Q1 also uses this feature */
 #if defined(CONFIG_MACH_U1) || defined(CONFIG_MACH_TRATS)
 #ifdef CONFIG_MACH_Q1_BD
 #define HW_OOB
-#endif /* CONFIG_MACH_Q1_BD */
+#endif
 #define USE_CID_CHECK
 #define WRITE_MACADDR
-#endif /* CONFIG_MACH_U1 || CONFIG_MACH_TRATS */
+#endif
 
 #ifdef CONFIG_ARCH_MSM7X30
 #define HW_OOB
 #define READ_MACADDR
-#endif /* CONFIG_ARCH_MSM7X30 */
+#endif
 
-#if defined(CONFIG_MACH_GC1) || defined(CONFIG_MACH_U1_NA_SPR) || defined(CONFIG_MACH_V1)
+#if defined CONFIG_MACH_GC1 || defined CONFIG_MACH_U1_NA_SPR
 #undef USE_CID_CHECK
 #define READ_MACADDR
-#endif /* CONFIG_MACH_GC1 || CONFIG_MACH_U1_NA_SPR || CONFIG_MACH_V1 */
+#endif
 
 #ifdef CONFIG_MACH_P10
 #define READ_MACADDR
-#endif /* CONFIG_MACH_P10 */
+#endif
 
 #ifdef CONFIG_ARCH_MSM8960
 #undef WIFI_TURNOFF_DELAY
 #define WIFI_TURNOFF_DELAY	200
-#endif /* CONFIG_ARCH_MSM8960 */
-
-#if defined(CONFIG_BCM4335) || defined (CONFIG_BCM4335_MODULE)
-#define POWERUP_MAX_RETRY 5 /* Due to late start-up of FPGA in JF project */
-#endif /* CONFIG_BCM4335 || CONFIG_BCM4335_MODULE */
+#endif
 
 /* REGION CODE */
-#ifndef CONFIG_WLAN_REGION_CODE
-#define CONFIG_WLAN_REGION_CODE 100
-#endif /* CONFIG_WLAN_REGION_CODE */
 
-#if (CONFIG_WLAN_REGION_CODE >= 100) && (CONFIG_WLAN_REGION_CODE < 200)     /* EUR */
-#if (CONFIG_WLAN_REGION_CODE == 101)     /* EUR ORG */
+#if (WLAN_REGION_CODE >= 100) && (WLAN_REGION_CODE < 200) /*EUR*/
+#if (WLAN_REGION_CODE == 101) /*EUR ORG*/
 /* GAN LITE NAT KEEPALIVE FILTER */
 #define GAN_LITE_NAT_KEEPALIVE_FILTER
-#endif /* CONFIG_WLAN_REGION_CODE == 101 */
-#endif /* CONFIG_WLAN_REGION_CODE >= 100 && CONFIG_WLAN_REGION_CODE < 200 */
+#endif
+#endif
 
-#if (CONFIG_WLAN_REGION_CODE >= 200) && (CONFIG_WLAN_REGION_CODE < 300)     /* KOR */
+#if (WLAN_REGION_CODE >= 200) && (WLAN_REGION_CODE < 300) /* KOR */
 #undef USE_INITIAL_2G_SCAN_ORG
 #ifndef ROAM_ENABLE
 #define ROAM_ENABLE
-#endif /* ROAM_ENABLE */
+#endif
 #ifndef ROAM_API
 #define ROAM_API
-#endif /* ROAM_API */
+#endif
 #ifndef ROAM_CHANNEL_CACHE
 #define ROAM_CHANNEL_CACHE
-#endif /* ROAM_CHANNEL_CACHE */
+#endif
 #ifndef OKC_SUPPORT
 #define OKC_SUPPORT
-#endif /* OKC_SUPPORT */
+#endif
 
 #ifndef ROAM_AP_ENV_DETECTION
 #define ROAM_AP_ENV_DETECTION
-#endif /* ROAM_AP_ENV_DETECTION */
+#endif
 
 #undef WRITE_MACADDR
 #undef READ_MACADDR
-#if defined(CONFIG_BCM4334) || defined(CONFIG_BCM4335) \
-	|| defined(CONFIG_BCM4334_MODULE) || defined(CONFIG_BCM4335_MODULE)
+#ifdef CONFIG_BCM4334
 #define READ_MACADDR
 #else
 #define RDWR_MACADDR
-#endif /* CONFIG_BCM4334 || CONFIG_BCM4335 */
-
-#if (CONFIG_WLAN_REGION_CODE == 201)     /* SKT */
-
-#ifdef CONFIG_MACH_UNIVERSAL5410
-// Make CPU core clock 300MHz & assign dpc thread workqueue to CPU1
-#define FIX_CPU_MIN_CLOCK
 #endif
 
-#endif /* CONFIG_WLAN_REGION_CODE == 201 */
+#if (WLAN_REGION_CODE == 201) /* SKT */
+#endif
 
-#if (CONFIG_WLAN_REGION_CODE == 202)     /* KTT */
+#if (WLAN_REGION_CODE == 202) /* KTT */
 #define VLAN_MODE_OFF
-#define CUSTOM_KEEP_ALIVE_SETTING   30000 /* JBP type KOR KTT only. do not correct here */
+#define KEEP_ALIVE_PACKET_PERIOD_30_SEC
 #define FULL_ROAMING_SCAN_PERIOD_60_SEC
-
-#ifdef CONFIG_MACH_UNIVERSAL5410
-// Make CPU core clock 300MHz & assign dpc thread workqueue to CPU1
-#define FIX_CPU_MIN_CLOCK
 #endif
 
-#endif /* CONFIG_WLAN_REGION_CODE == 202 */
-
-#if (CONFIG_WLAN_REGION_CODE == 203)     /* LGT */
-#ifdef CONFIG_MACH_UNIVERSAL5410
-// Make CPU core clock 300MHz & assign dpc thread workqueue to CPU1
-#define FIX_CPU_MIN_CLOCK
-#define FIX_BUS_MIN_CLOCK
+#if (WLAN_REGION_CODE == 203) /* LGT */
 #endif
-#endif /* CONFIG_WLAN_REGION_CODE == 203 */
-#endif /* CONFIG_WLAN_REGION_CODE >= 200 && CONFIG_WLAN_REGION_CODE < 300 */
+#endif
 
-#if (CONFIG_WLAN_REGION_CODE >= 300) && (CONFIG_WLAN_REGION_CODE < 400)     /* CHN */
-#ifndef BCMWAPI_WPI
+#if (WLAN_REGION_CODE >= 300) && (WLAN_REGION_CODE < 400) /* CHN */
 #define BCMWAPI_WPI
-#endif
-#ifndef BCMWAPI_WAI
 #define BCMWAPI_WAI
 #endif
-#endif /* CONFIG_WLAN_REGION_CODE >= 300 && CONFIG_WLAN_REGION_CODE < 400 */
 
-#if (CONFIG_WLAN_REGION_CODE >= 400) && (CONFIG_WLAN_REGION_CODE < 500)     /* JPN */
-#define DISABLE_11AC
-#endif /* CONFIG_WLAN_REGION_CODE >= 400 && CONFIG_WLAN_REGION_CODE < 500 */
-
-#if !defined(READ_MACADDR) && !defined(WRITE_MACADDR) \
-	&& !defined(RDWR_KORICS_MACADDR) && !defined(RDWR_MACADDR)
+#if !defined(READ_MACADDR) && !defined(WRITE_MACADDR) && !defined(RDWR_KORICS_MACADDR) && !defined(RDWR_MACADDR)
 #define GET_MAC_FROM_OTP
-#define SHOW_NVRAM_TYPE
-#endif /* !READ_MACADDR && !WRITE_MACADDR && !RDWR_KORICS_MACADDR && !RDWR_MACADDR */
+#endif
 
-#endif /* _dhd_sec_feature_h_ */

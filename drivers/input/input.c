e80cc90d08a53afb9edd83de3733b84627e0be92
googyanas <googyanas@gmail.com>
2013-05-31 11:08:45 +0100

    Touchwake fix (disabled if incall)

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -27,6 +27,7 @@
 #include <linux/mutex.h>
 #include <linux/rcupdate.h>
 #include "input-compat.h"
+#include <linux/telephony.h>
 
 #ifdef CONFIG_TOUCH_WAKE
 #include <linux/touch_wake.h>
@@ -251,7 +252,7 @@ static void input_handle_event(struct input_dev *dev,
 		if (is_event_supported(code, dev->keybit, KEY_MAX) &&
 		    !!test_bit(code, dev->key) != value) {
 #ifdef CONFIG_TOUCH_WAKE
-      if (code == KEY_POWER && !device_is_suspended()) {
+      if (code == KEY_POWER && !device_is_suspended() && !PSTN_ON_HOOK ) {
         if (value == 1) {
           powerkey_pressed();
         }

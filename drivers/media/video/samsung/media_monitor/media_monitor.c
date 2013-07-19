65ddbdd887f53e5563428ae786b8f6fe8c7b880d
googyanas <googyanas@gmail.com>
2013-07-19 18:46:41 +0000

    Other tweaks

--- drivers/media/video/samsung/media_monitor/media_monitor.c
+++ drivers/media/video/samsung/media_monitor/media_monitor.c
@@ -29,9 +29,9 @@ struct mhs_context {
 };
 
 //TODO Replace this with a proper notifier chain in the future
-//#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
+#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
 extern void set_mic_level(void);
-//#endif
+#endif
 // extern void do_mdnie_refresh(struct work_struct *work);
 
 void mhs_set_status(enum mhs_type type, bool status)
@@ -43,9 +43,9 @@ void mhs_set_status(enum mhs_type type, bool status)
 //					do_mdnie_refresh(NULL); //TODO see above
 					break;
 		case MHS_CAMERA_STREAM:	mhs_ctx.camera_stream = status;
-//#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
+#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
 					set_mic_level(); //TODO see above
-//#endif
+#endif
 					break;
 		default:		return;
 	}

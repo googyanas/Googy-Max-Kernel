ff74d37753d321c7a76105ec4366cf9ed856e353
googyanas <googyanas@gmail.com>
2013-07-24 16:50:23 +0000

    Dynamic FSync update v1.2

--- fs/sync.c
+++ fs/sync.c
@@ -170,14 +170,15 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-	struct address_space *mapping = file->f_mapping;
-	int err, ret;
-
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
+
+	struct address_space *mapping = file->f_mapping;
+	int err, ret;
+
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
 		goto out;
@@ -233,7 +234,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -243,7 +244,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -318,7 +319,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -422,7 +423,7 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif

9ac3bc6f51a7d84455f7d6f7188aad40951ad0e0
googyanas <googyanas@gmail.com>
2013-08-03 17:05:29 +0000

    Build_Kernel : Rearrange the scipt

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,9 +4,10 @@ export RAMFS_SOURCE=`readlink -f $KERNELDIR/../ramfs-sgs3`
 export PARENT_DIR=`readlink -f ..`
 export USE_SEC_FIPS_MODE=true
 # export CROSS_COMPILE=$PARENT_DIR/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-# export CROSS_COMPILE=/home/googy/Desktop/arm-2009q3/bin/arm-none-linux-gnueabi-
-# export CROSS_COMPILE=/home/googy/Desktop/arm-linaro2/bin/arm-linux-gnueabihf-
-export CROSS_COMPILE=/usr/bin/arm-linux-gnueabi-
+# export CROSS_COMPILE=/home/googy/Bureau/arm-2009q3/bin/arm-none-linux-gnueabi-
+# export CROSS_COMPILE=/home/googy/Bureau/arm-linaro2/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf-
+# export CROSS_COMPILE=/home/googy/Bureau/arm-linaro4/bin/arm-gnueabi-
 
 if [ "${1}" != "" ];then
   export KERNELDIR=`readlink -f ${1}`
@@ -16,7 +17,7 @@ RAMFS_TMP="/home/googy/tmp/ramfs-source-sgs3"
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make -j5 0googymax_defconfig KALLSYMS_EXTRA_PASS=1
+  make -j4 0googymax_defconfig
 fi
 
 . $KERNELDIR/.config
@@ -24,7 +25,7 @@ fi
 export ARCH=arm
 
 cd $KERNELDIR/
-make -j5 KALLSYMS_EXTRA_PASS=1 || exit 1
+make -j4 || exit 1
 
 #remove previous ramfs files
 rm -rf $RAMFS_TMP
@@ -41,10 +42,10 @@ rm -rf $RAMFS_TMP/tmp/*
 rm -rf $RAMFS_TMP/.hg
 #copy modules into ramfs
 mkdir -p $INITRAMFS/lib/modules
-mv -f drivers/media/video/samsung/mali_r3p0_lsi/mali.ko drivers/media/video/samsung/mali_r3p0_lsi/mali_r3p0_lsi.ko
-mv -f drivers/net/wireless/bcmdhd.cm/dhd.ko drivers/net/wireless/bcmdhd.cm/dhd_cm.ko
+# mv -f drivers/media/video/samsung/mali_r3p0_lsi/mali.ko drivers/media/video/samsung/mali_r3p0_lsi/mali_r3p0_lsi.ko
+# mv -f drivers/net/wireless/bcmdhd.cm/dhd.ko drivers/net/wireless/bcmdhd.cm/dhd_cm.ko
 find -name '*.ko' -exec cp -av {} $RAMFS_TMP/lib/modules/ \;
-/usr/bin/arm-linux-gnueabi-strip --strip-unneeded $RAMFS_TMP/lib/modules/*
+/usr/bin/arm-linux-gnueabihf-strip --strip-unneeded $RAMFS_TMP/lib/modules/*
 
 cd $RAMFS_TMP
 find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
@@ -52,14 +53,14 @@ ls -lh $RAMFS_TMP.cpio
 gzip -9 $RAMFS_TMP.cpio
 cd -
 
-make -j5 zImage KALLSYMS_EXTRA_PASS=1 || exit 1
+make -j4 zImage || exit 1
 
 ./mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.gz --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNELDIR/boot.img.pre
 
 $KERNELDIR/mkshbootimg.py $KERNELDIR/boot.img $KERNELDIR/boot.img.pre $KERNELDIR/payload.tar
 # rm -f $KERNELDIR/boot.img.pre
 
-cd /home/googy/Desktop/Sources/Googy-Max-Kernel
-mv -f -v /home/googy/Desktop/Sources/Googy-Max-Kernel/Kernel/boot.img .
+cd /home/googy/Bureau/Sources/Googy-Max-Kernel
+mv -f -v /home/googy/Bureau/Sources/Googy-Max-Kernel/Kernel/boot.img .
 cp -f -v Googy-Max-Kernel_0.zip Googy-Max-Kernel_TEST_CWM.zip
 zip -v Googy-Max-Kernel_TEST_CWM.zip boot.img

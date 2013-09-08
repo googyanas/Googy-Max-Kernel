308d49ec9c282646f831f212ba29171025b46ff5
googyanas <googyanas@gmail.com>
2013-09-08 21:20:20 +0000

    mmc core : increase limit_us

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -571,9 +571,9 @@ void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card)
 			 * previous value of 300ms is known to be
 			 * insufficient for some cards.
 			 */
-			limit_us = 300000;
+			limit_us = 1500000;
 		else
-			limit_us = 100000;
+			limit_us = 500000;
 
 		/*
 		 * SDHC cards always use these fixed values.
